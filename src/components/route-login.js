8a9eb6d9c47f0b4ed8fd207d0e6aa6c85598734c
Atik Fahad <fahadatik@gmail.com>
2020-03-28 19:29:42 +0600

    finish doing auth and dashboard

--- src/components/route-login.js
+++ src/components/route-login.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import { useIdentityContext } from 'react-netlify-identity-widget';
+import { navigate } from 'gatsby';
 
-const RouteLogin = () => (
-  <>
-    <h1>Log In or Sign Up</h1>
-    <button>Log In</button>
-  </>
-);
-
+const RouteLogin = ({ showModal }) => {
+  const identity = useIdentityContext();
+  if (identity && identity.isLoggedIn) {
+    navigate('/dashboard/secreet', { replace: true });
+  }
+  return (
+    <>
+      <h1>Log In or Sign Up</h1>
+      <button onClick={showModal}>Log In</button>
+    </>
+  );
+};
 export default RouteLogin;
